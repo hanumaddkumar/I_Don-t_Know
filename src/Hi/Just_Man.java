f33969b8aa987727a664a1a414991bed4f90a9fb
hanumakumar-dxc <68776383+hanumakumar-dxc@users.noreply.github.com>
2021-07-26 11:56:23 +0530

    Commit

---
+++ src/Hi/Just_Man.java
@@ -0,0 +1,21 @@
+package Hi;
+
+import java.util.Scanner;
+
+public class Just_Man {
+	public static void main(String[] args)
+	{
+	   Scanner s = new Scanner(System.in);
+	   String D="A Stone of USA and INDIA";
+	   int a=s.nextInt();
+	   if(a>30)
+	   {
+		   System.out.print("Employee Name is in Records");
+	   }
+	   else
+	   {
+		   System.out.println("Employee Name is Not in Records");
+	   }
+
+    }
+}
